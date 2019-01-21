ed3d80b489fca604ee3be3b08b6ad0e93d2767a7
Talouchy <danyal.lakzian@gmail.com>
2019-01-21 13:58:13 +1100

    fix: changed the config directory

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const serverConfig  = require('./config/config.js')
+const serverConfig  = require('../config/config.js')
 
 app.set('port', serverConfig.port)
 
