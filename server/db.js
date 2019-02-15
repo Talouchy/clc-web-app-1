ec26e026770b35944d01cc81f4d9757d26e5a1de
Talouchy <danyal.lakzian@gmail.com>
2019-02-16 06:21:17 +1100

    update: updated the database file

--- server/db.js
+++ server/db.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose')
+const consola = require('consola')
 const { serverConfig, dbConfig } = require('../config/config')
 
+const url = `mongodb://localhost/${dbConfig.db_name}`
 
-const url = `mongodb://${dbConfig.db_username}:${dbConfig.db_password}@${dbConfig.db_host}:${dbConfig.db_port}/${dbConfig.db_name}`
-
-mongoose.Promise = global.Promise;
+var options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
 
 // connect to mongo DB
-mongoose.connect(url, { useNewUrlParser: true }, () => console.log('Mongo is connected') )
+mongoose.connect(url, options, () => consola.success('Mongo is connected') )
+
+// get mongoose to use the global promise library
+mongoose.Promise = global.Promise
 
 if (serverConfig.app === 'development')
   mongoose.set('debug', true)
 
-module.exports = {
-  connection: mongoose.connection,
-}
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+module.exports = db
