82971582c2e9c3ed69a2e0a843a7ddca112f05d0
Talouchy <danyal.lakzian@gmail.com>
2019-02-27 11:36:18 +1100

    update: added alert banner & recaptcha functionality

--- pages/feedback.vue
+++ pages/feedback.vue
@@ -42,13 +42,41 @@
           pb-0
           pt-0
           mt-5>
-          <v-textarea
+          <v-alert
+            :value="alert.status"
+            :icon="alert.icon"
+            :color="alert.type"
+            transition="scale-transition"
             outline
-            label="Feedback"
-            value=""
-            placeholder="Write your feedback here..."
-            color="purple darken-2"
-            height="300px" />
+            style="font-size: x-large">
+            {{ alert.msg }}
+          </v-alert>
+          <form
+            ref="feedbackForm"
+            action="api/feedbacks"
+            method="post">
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="E-mail"
+              outline
+              placeholder="example@example.com"
+              color="purple darken-2"
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+              @click="resetForm"/>
+            <v-textarea
+              v-model="feedbackMsg"
+              :error-messages="feedbackErrors"
+              outline
+              label="Feedback"
+              placeholder="Write your feedback here..."
+              color="purple darken-2"
+              height="300px"
+              @input="$v.feedbackMsg.$touch()"
+              @blur="$v.feedbackMsg.$touch()"
+              @click="resetForm"/>
+          </form>
         </v-flex>
       </v-layout>
       <v-layout
@@ -63,21 +91,134 @@
             round
             color="primary"
             dark
-            class="right">
+            class="right"
+            @click="executeRecaptcha">
             Send Feedback
           </v-btn>
+          <recaptcha
+            ref="recaptcha"
+            @verify="submit" />
         </v-flex>
       </v-layout>
     </v-container>
   </v-layout>
 </template>
 
+<script type="text/javascript">
+var onloadCallback = function() {
+  alert('grecaptcha is ready!')
+  this.render()
+}
+window.onloadCallback = onloadCallback()
+</script>
+
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, email } from 'vuelidate/lib/validators'
+import axios from 'axios'
+import Recaptcha from '~/components/recaptcha'
+
 export default {
   name: 'Feedback',
-  data: () => ({}),
+  components: {
+    recaptcha: Recaptcha
+  },
+  mixins: [validationMixin],
+  validations: {
+    feedbackMsg: {
+      required,
+      maxLength: maxLength(300)
+    },
+    email: {
+      required,
+      email
+    }
+  },
+  data: () => ({
+    feedbackMsg: '',
+    email: '',
+    alert: {
+      status: false,
+      icon: 'check_circle',
+      type: 'success',
+      msg: 'Feedback sent. Thank you.'
+    }
+  }),
+  computed: {
+    feedbackErrors() {
+      const errors = []
+      if (!this.$v.feedbackMsg.$dirty) return errors
+      !this.$v.feedbackMsg.maxLength &&
+        errors.push('Feedback must be at most 300 characters long')
+      !this.$v.feedbackMsg.required &&
+        errors.push('Feedback message is required')
+      return errors
+    },
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    }
+  },
+  mounted() {},
   created: function() {
     this.$nuxt.$emit('updatePageTitle', 'Feedback')
+  },
+  methods: {
+    executeRecaptcha() {
+      this.$refs.recaptcha.execute()
+    },
+    submit(recaptchaToken) {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.alert.status = true
+        this.alert.type = 'error'
+        this.alert.icon = 'warning'
+        this.alert.msg = 'Error in sending the feedback'
+      } else {
+        axios
+          .post('/api/recaptcha/verify', {
+            email: self.email,
+            feedbackMsg: self.feedbackMsg,
+            recaptchaToken: recaptchaToken
+          })
+          .then(response => {
+            console.log(response)
+            axios
+              .post('/api/feedbacks', {
+                message: this.feedbackMsg,
+                userID: '1234',
+                email: this.email
+              })
+              .then(response => {
+                console.log(response)
+                this.alert.status = true
+                this.alert.type = 'success'
+                this.alert.icon = 'check_circle'
+                this.alert.msg = 'Feedback sent. Thank you for your feedback.'
+              })
+              .catch(error => {
+                console.log(error)
+                this.alert.status = true
+                this.alert.type = 'error'
+                this.alert.icon = 'warning'
+                this.alert.msg = 'Error in sending the feedback'
+              })
+          })
+          .catch(err => {
+            this.alert.status = true
+            this.alert.type = 'error'
+            this.alert.icon = 'warning'
+            this.alert.msg = 'Error verifying ReCaptcha!'
+          })
+      }
+    },
+    resetForm() {
+      this.$v.$reset
+      this.alert.status = false
+    }
   }
 }
 </script>
