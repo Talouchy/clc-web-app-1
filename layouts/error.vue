d674b0a475c12c63939e4e00f40573f00999fe83
Talouchy <danyal.lakzian@gmail.com>
2019-03-02 03:44:02 +1100

    update: changed error page layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,33 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+  <v-container grid-list-md>
+    <v-layout
+      row
+      wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title
+            v-if="error.statusCode"
+            class="error darken-3 white--text subheading"
+            primary-title>
+            <span class="title">Oops! => Error code: {{ error.statusCode }}</span>
+          </v-card-title>
+          <v-card-text v-if="error.message">
+            {{ error.message }}
+          </v-card-text>
+          <v-card-text v-else>
+            Looks like you're in uncharted territory. We don't know about this page yet.
+            Luckily, we know the way back.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              flat
+              class="error darken-3 white--text"
+              to="/">Home</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
